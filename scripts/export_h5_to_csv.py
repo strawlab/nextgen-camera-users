e804ffb1e46bf64905760a0770b7ca4888b88250
Andrew Straw <strawman@astraw.com>
2020-07-01 00:19:28 +0200

    format with black

--- scripts/export_h5_to_csv.py
+++ scripts/export_h5_to_csv.py
@@ -13,7 +13,7 @@ import flydra_analysis.a2.calibration_to_xml as calibration_to_xml
 
 fname = sys.argv[1]
 outdir, ext = os.path.splitext(fname)
-assert ext == '.h5' or ext == '.hdf5'
+assert ext == ".h5" or ext == ".hdf5"
 
 try:
     os.makedirs(outdir)
@@ -27,21 +27,23 @@ with tables.open_file(fname) as h5:
     # 2d data ------
     d2d = h5.root.data2d_distorted[:]
     df = pandas.DataFrame(d2d)
-    csv_fname = os.path.join(outdir, 'data2d_distorted.csv')
-    df.to_csv(csv_fname, index=False, float_format='%r')
+    csv_fname = os.path.join(outdir, "data2d_distorted.csv")
+    df.to_csv(csv_fname, index=False, float_format="%r")
 
     # cam info ------
     cam_info = h5.root.cam_info[:]
     df = pandas.DataFrame(cam_info)
-    df["cam_id"] = df["cam_id"].str.decode('ascii')
-    csv_fname = os.path.join(outdir, 'cam_info.csv')
-    df.to_csv(csv_fname, index=False, float_format='%r')
+    df["cam_id"] = df["cam_id"].str.decode("ascii")
+    csv_fname = os.path.join(outdir, "cam_info.csv")
+    df.to_csv(csv_fname, index=False, float_format="%r")
+
 
 class Options:
     pass
 
+
 options = Options()
 options.scaled = False
-options.dest = os.path.join(outdir, 'calibration.xml')
+options.dest = os.path.join(outdir, "calibration.xml")
 
 calibration_to_xml.doit(fname, options)
